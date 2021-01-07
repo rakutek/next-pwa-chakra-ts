ba81b25d04588d3a9eecec777e519abb1c72a82b
rakutek <rakutek12@gmail.com>
2021-01-07 18:38:24 +0900

    add _document

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import NextDocument, { Html, Head, Main } from 'next/document'
+
+export default class Document extends NextDocument {
+  render(): JSX.Element {
+    return (
+      <Html lang="ja-JP">
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
+        <body>
+          <Main />
+        </body>
+      </Html>
+    )
+  }
+}
